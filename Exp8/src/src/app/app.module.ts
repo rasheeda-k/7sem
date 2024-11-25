a337ffa766c9ab50ce374a53160d1649d43e3b18
rasheeda-k <rasheedakarumanchi@gmail.com>
2024-11-25 06:10:39 +0530

    Add files via upload

---
+++ Exp8/src/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
+  ],
+  providers: [
+    provideClientHydration(),
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
